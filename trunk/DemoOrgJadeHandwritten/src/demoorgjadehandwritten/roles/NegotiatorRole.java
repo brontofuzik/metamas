e6b22ab63f0496a3367d7cf2408e3cc8e3af5b0c
lukas.kudela@gmail.com <lukas.kudela@gmail.com@918c96d0-4712-a5f2-c2c5-4549124dd8aa>
2011-10-11 17:11:09 +0000

    - added agents, groups and roles.

---
+++ trunk/DemoOrgJadeHandwritten/src/demoorgjadehandwritten/roles/NegotiatorRole.java
@@ -0,0 +1,20 @@
+package demoorgjadehandwritten.roles;
+
+import jadeorg.Role;
+
+/**
+ *
+ * @author Lukáš Kúdela (2011-10-11)
+ */
+public class NegotiatorRole extends Role {
+
+    @Override
+    public void action() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean done() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }   
+}
---
+++ trunk/JadeOrg/src/jadeorg/Role.java
@@ -0,0 +1,11 @@
+package jadeorg;
+
+import jade.core.behaviours.Behaviour;
+
+/**
+ *
+ * @author Lukáš Kúdela (2011-10-11)
+ */
+public abstract class Role extends Behaviour {
+    
+}
