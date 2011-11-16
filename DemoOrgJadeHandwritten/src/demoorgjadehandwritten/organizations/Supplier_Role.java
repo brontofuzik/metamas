f77c7476139f3c4001c7ca69facafb7abe97a246
lukas.kudela <lukas.kudela@gmail.com>
2011-11-16 11:22:08 +0000

    - updated the DemoOrgJadeHandwritten project to use the new JadeOrg classes.

---
+++ DemoOrgJadeHandwritten/src/demoorgjadehandwritten/organizations/Supplier_Role.java
@@ -0,0 +1,10 @@
+package demoorgjadehandwritten.organizations;
+
+import jadeorg.core.organization.Role;
+
+/**
+ *
+ * @author Lukáš Kúdela (2011-10-11)
+ */
+public class Supplier_Role extends Role {
+}
