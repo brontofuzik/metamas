2380b23195bfa9c682df1acfd1defcaef98a7d2d
lukas.kudela@gmail.com <lukas.kudela@gmail.com@918c96d0-4712-a5f2-c2c5-4549124dd8aa>
2011-11-16 11:22:08 +0000

    - updated the DemoOrgJadeHandwritten project to use the new JadeOrg classes.

---
+++ trunk/DemoOrgJadeHandwritten/src/demoorgjadehandwritten/organizations/Ringleader_Role.java
@@ -0,0 +1,10 @@
+package demoorgjadehandwritten.organizations;
+
+import jadeorg.core.organization.Role;
+
+/**
+ *
+ * @author Lukáš Kúdela (2011-10-11)
+ */
+public class Ringleader_Role extends Role {
+}
---
+++ trunk/DemoOrgJadeHandwritten/src/demoorgjadehandwritten/organizations/WesternSociety_Organization.java
@@ -0,0 +1,11 @@
+package demoorgjadehandwritten.organizations;
+
+import jadeorg.core.organization.Organization;
+
+/**
+ *
+ * @author Lukáš Kúdela (2010-10-10)
+ */
+public class WesternSociety_Organization extends Organization {
+    
+}
