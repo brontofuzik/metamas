469e3047e76749c0d46a0559dba9b7dd6299f7a8
lukas.kudela@gmail.com <lukas.kudela@gmail.com@918c96d0-4712-a5f2-c2c5-4549124dd8aa>
2011-11-21 23:12:26 +0000

    AuctionJadeHandwritten: - worked on the Participant player.

--- trunk/AuctionJadeHandwritten/src/auctionjadehandwritten/players/Participant_Player.java
+++ trunk/AuctionJadeHandwritten/src/auctionjadehandwritten/players/Participant_Player.java
@@ -1,6 +1,8 @@
 package auctionjadehandwritten.players;
 
 import jadeorg.core.player.Player;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * A Participant player.
@@ -10,11 +12,63 @@ import jadeorg.core.player.Player;
  */
 public class Participant_Player extends Player {
 
+    // <editor-fold defaultstate="collapsed" desc="Fields">
+    
+    private static Set<String> abilities = new HashSet<String>();
+    
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Constructors">
+    
+    static {
+        abilities.add("task"); // Auctioneer
+        abilities.add("propose"); // Bidder
+        abilities.add("evaluate"); // Auctioneer
+        abilities.add("execute"); // Bidder
+    }
+    
+    // </editor-fold>
+    
     // <editor-fold defaultstate="collapsed" desc="Methods">
     
+    /**
+     * Evaluates a set of requirements.
+     * @param requirements the set of requirements to evaluate
+     * @return <c>true</c> if all requirements can be met; <c>false</c> otherwise
+     */
     @Override
     protected boolean evaluateRequirements(String[] requirements) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        for (String requirement : requirements) {
+            if (evaluateRequirement(requirement)) {
+                return false;
+            }
+        }
+        return true;
+    }
+  
+    /**
+     * Evaluates a set of requirements.
+     * @param requirements the set of requirements to evaluate
+     * @return <c>true</c> if any requirement can be met; <c>false</c> otherwise
+     */
+    protected boolean evaluteAnyRequirement(String[] requirements) {
+        for (String requirement : requirements) {
+            if (evaluateRequirement(requirement)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    // ---------- PRIVATE ----------
+    
+    /**
+     * Evaluates a requirement.
+     * @param requirement the requirement to evaluate
+     * @return <c>true</c> if all requirements can be met; <c>false</c> otherwise 
+     */
+    private boolean evaluateRequirement(String requirement) {
+        return abilities.contains(requirement);
     }
     
     // </editor-fold>
