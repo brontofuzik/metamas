93b00cd68e30a695c458a3aea3fe717507b74f84
lukas.kudela <lukas.kudela@gmail.com>
2011-11-25 13:01:27 +0000

    DemoOrgJadeHandwritten: - modified the run.bat file.

--- DemoOrgJadeHandwritten/bin/run.bat
+++ DemoOrgJadeHandwritten/bin/run.bat
@@ -2,35 +2,43 @@
 
 REM Solution directory for HP
 SET SOLUTION_DIR=C:\DATA\projects\MAS\MetaMAS
+
 REM Solution directory for Prestigio
 REM SET SOLUTION_DIR=D:\projects\MAS\MetaMAS
+
 SET PROJECT_NAME=DemoOrgJadeHandwritten
 SET PROJECT_DIR=%SOLUTION_DIR%\%PROJECT_NAME%
 SET JADE_VERSION=4.1
 
 REM ----- CLASSPATH -----
-SET JADE_JAR=%PROJECT_DIR%\dist\lib\jade.jar
-SET JADEORG_JAR=%PROJECT_DIR%\dist\lib\JadeOrg.jar
+SET JADE_JAR=%SOLUTION_DIR%\lib\Jade\%JADE_VERSION%\jade.jar
+SET JADEORG_JAR=%SOLUTION_DIR%\JadeOrg\dist\JadeOrg.jar
 SET PROJECT_JAR=%PROJECT_DIR%\dist\%PROJECT_NAME%.jar
 SET CLASSPATH=%JADE_JAR%;%JADEORG_JAR%;%PROJECT_JAR%
 
-REM ----- Groups -----
-SET PACKAGE=demoorgjadehandwritten.groups
-SET GROUP1=terroristOrganizationGroup:%PACKAGE%.TerroristOrganizationGroup("Terrorist organization [group]")
-SET GROUP2=weaponsCartelGroup:%PACKAGE%.WeaponsCartelGroup("Weapons cartel [group]")
-SET GROUP3=westernSocietyGroup:%PACKAGE%.WesternSocietyGroup("Western society [group]")
-
 REM ----- Agents -----
-SET PACKAGE=demoorgjadehandwritten.agents
-SET AGENT1=agentA:%PACKAGE%.HumanAgent("Agent A [agent]",terroristOrganizationGroup.Operative,weaponsCartelGroup.Customer,westerSocietyGroup.Student)
-SET AGENT2=agentB:%PACKAGE%.HumanAgent("Agent B [agent]",weaponsCartelGroup.Negotiator)
-SET AGENT3=agentC:%PACKAGE%.HumanAgent("Agent C [agent]",weaponsCartelGroup.Supplier)
-SET AGENT4=agentD:%PACKAGE%.HumanAgent("Agent D [agent]",westernSocietyGroup.Citizen)
-SET AGENT5=agentE:%PACKAGE%.HumanAgent("Agent E [agent]",terroristOrganizationGroup.Ringleader)
+SET ROOT_NAMESPACE=demoorgjadehandwritten
 
+REM ----- Organizations -----
+SET ORGANIZATION_PACKAGE=%ROOT_NAMESPACE%.organizations
+SET TERRORIST_ORGANIZATION1_ORGANIZATION=terroristOrganization1_Organization:%ORGANIZATION_PACKAGE%.TerroristOrganization_Organization
+SET WEAPONS_CARTEL1_ORGANIZATION=weaponsCartler1_Organization:%ORGANIZATION_PACKAGE%.WeaponsCartel_Organization
+SET WESTERN_SOCIETY1_ORGANIZATION=westernSociety1_Organization:%ORGANIZATION_PACKAGE%.WesternSociety_Organization
+SET ORGANIZATIONS=%TERRORIST_ORGANIZATION1_ORGANIZATION%;%WEAPONS_CARTEL1_ORGANIZATION%;%WESTERN_SOCIETY1_ORGANIZATION%
+
+REM ----- Players -----
+SET PLAYER_PACKAGE=%ROOT_NAMESPACE%.players
+SET AGENT_A_PLAYER=agentA:%PLAYER_PACKAGE%.Human_Player(terroristOrganizationGroup.Operative,weaponsCartelGroup.Customer,westerSocietyGroup.Student)
+SET AGENT_B_PLAYER=agentB:%PLAYER_PACKAGE%.Human_Player(weaponsCartelGroup.Negotiator)
+SET AGENT_C_PLAYER=agentC:%PLAYER_PACKAGE%.Human_Player(weaponsCartelGroup.Supplier)
+SET AGENT_D_PLAYER=agentD:%PLAYER_PACKAGE%.Human_Player(westernSocietyGroup.Citizen)
+SET AGENT_E_PLAYER=agentE:%PLAYER_PACKAGE%.Human_Player(terroristOrganizationGroup.Ringleader)
+SET PLAYERS=%AGENT_A_PLAYER%;%AGENT_B_PLAYER%;%AGENT_C_PLAYER%;%AGENT_D_PLAYER%;%AGENT_E_PLAYER%
+
+SET LOGGING_CONFIG_FILE=%PROJECT_DIR%\logging.properties
 SET OPTIONS=-gui
-SET AGENTS=%GROUP1%;%GROUP2%;%GROUP3%;%AGENT1%;%AGENT2%;%AGENT3%;%AGENT4%;%AGENT5%
+SET AGENTS=%ORGANIZATIONS%;%PLAYERS%
 
 @ECHO ON
 
-java -classpath %CLASSPATH% jade.Boot %OPTIONS% %AGENTS%
+java -classpath %CLASSPATH% -Djava.util.logging.config.file=%LOGGING_CONFIG_FILE% jade.Boot %OPTIONS% %AGENTS%
