bda590b1ef89e09a38c3076aa85ae013926457e5
lukas.kudela@gmail.com <lukas.kudela@gmail.com@918c96d0-4712-a5f2-c2c5-4549124dd8aa>
2011-12-24 01:35:51 +0000

    JadeOrg: - added the 'Send success or failure' multi sender state.

---
+++ trunk/JadeOrg/src/jadeorg/proto/SendSuccessOrFailure.java
@@ -0,0 +1,69 @@
+package jadeorg.proto;
+
+import jade.core.AID;
+
+/**
+ * A 'Send success or failure' multi sender state.
+ * @author Lukáš Kúdela
+ * @since 2011-12-24
+ * @version %I% %G%
+ */
+public abstract class SendSuccessOrFailure extends MultiSenderState {
+    
+    // <editor-fold defaultstate="collapsed" desc="Constant fields">
+    
+    // ----- Exit values -----
+    public static final int SUCCESS = 1;
+    public static final int FAILURE = 2;
+    // -----------------------
+    
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Constructors">
+    
+    protected SendSuccessOrFailure(String name, AID receiverAID) {
+        super(name);
+        
+        addSender(SUCCESS, new SendSuccess(receiverAID));
+        addSender(FAILURE, new SendFailure(receiverAID));
+        buildFSM();
+    }
+    
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Methods">
+    
+    protected abstract void onSuccessSender();
+    
+    // </editor-fold>  
+    
+    // <editor-fold defaultstate="collapsed" desc="Classes">
+    
+    private class SendSuccess extends InnerSenderState {
+
+        // <editor-fold defaultstate="collapsed" desc="Constant fields">
+        
+        private static final String NAME = "sender";
+        
+        // </editor-fold>
+        
+        // <editor-fold defaultstate="collapsed" desc="Constructors">
+        
+        SendSuccess(AID receiverAID) {
+            super(NAME, receiverAID);
+        }
+        
+        // </editor-fold>
+        
+        // <editor-fold defaultstate="collapsed" desc="Methods">
+        
+        @Override
+        public void action() {
+            onSuccessSender();
+        }
+    
+        // </editor-fold>
+    }
+    
+    // </editor-fold> 
+}
