318e0b689513d78b59737341fab949260f41fbbd
lukas.kudela <lukas.kudela@gmail.com>
2012-01-01 15:56:30 +0000

    Demo1: - initial commit.

---
+++ Demo1/src/demo1/players/DemoPlayer1.java
@@ -0,0 +1,18 @@
+package demo1.players;
+
+/**
+ * The DemoPlayer1 player. The player playing the Asker role.
+ * @author Lukáš Kúdela
+ * @since 2011-12-31
+ * @version %I% %G%
+ */
+public class DemoPlayer1 extends DemoPlayer {
+    
+    // <editor-fold defaultstate="collapsed" desc="Constructors">
+    
+    public DemoPlayer1() {
+        super(new PowerFullName("demoOrganization.Asker.compute-factorial"));
+    }
+    
+    // </editor-fold>
+}
