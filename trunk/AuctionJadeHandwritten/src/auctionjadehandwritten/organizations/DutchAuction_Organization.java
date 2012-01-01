4483aca0852e2932866702de9a95889c64c2ff10
lukas.kudela@gmail.com <lukas.kudela@gmail.com@918c96d0-4712-a5f2-c2c5-4549124dd8aa>
2012-01-01 17:08:52 +0000

    - renamed the AuctionJadeHandwritten project to 'Auction'.

---
+++ trunk/AuctionJadeHandwritten/src/auctionjadehandwritten/organizations/DutchAuction_Organization.java
@@ -0,0 +1,31 @@
+package auctionjadehandwritten.organizations;
+
+import jadeorg.core.organization.Organization;
+import jadeorg.core.organization.Role;
+
+/**
+ * A Dutch auction organization.
+ * @author Lukáš Kúdela
+ * @since 2011-11-20
+ * @version %I% %G%
+ */
+public class DutchAuction_Organization extends Organization {
+   
+    /**
+     * An Auctioneer role.
+     * @author Lukáš Kúdela
+     * @since 2011-11-20
+     * @version %I% %G%
+     */
+    private class Auctioneer_Role extends Role {
+    }
+    
+    /**
+     * A Bidder role.
+     * @author Lukáš Kúdela
+     * @since 2011-11-20
+     * @version %I% %G%
+     */
+    private class Bidder_Role extends Role {
+    }
+}
