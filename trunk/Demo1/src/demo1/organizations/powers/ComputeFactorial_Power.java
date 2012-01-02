55d68371a80accf33357baf0dda8037e4963f0d3
lukas.kudela@gmail.com <lukas.kudela@gmail.com@918c96d0-4712-a5f2-c2c5-4549124dd8aa>
2012-01-02 00:47:25 +0000

    Demo1: - debugged the 'Invoke power' protocol.

--- trunk/Demo1/src/demo1/organizations/powers/ComputeFactorial_Power.java
+++ trunk/Demo1/src/demo1/organizations/powers/ComputeFactorial_Power.java
@@ -1,6 +1,8 @@
 package demo1.organizations.powers;
 
 import jadeorg.core.organization.Power;
+import jadeorg.proto.jadeextensions.OneShotBehaviourState;
+import jadeorg.proto.jadeextensions.State;
 
 /**
  * The 'Compute factorial' power.
@@ -20,6 +22,45 @@ public class ComputeFactorial_Power extends Power {
     
     public ComputeFactorial_Power() {
         super(NAME);
+        
+        registerStatesAndTransitions();
+    }
+    
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Methods">
+    
+    private void registerStatesAndTransitions() {
+        // ----- States -----
+        State state1 = new State1();
+        // ------------------
+        
+        // Register the states.
+        registerFirstState(state1);
+        registerLastState(state1);
+        
+        // Register the transitions.
+        // No transitions.
+    }
+    
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Classes">
+    
+    private class State1 extends OneShotBehaviourState {
+
+        private static final String NAME = "state1";
+        
+        State1() {
+            super(NAME);
+        }
+        
+        @Override
+        public void action() {
+            Object argument = (Integer)getArgument();
+            setResult(argument);
+        }
+    
     }
     
     // </editor-fold>
