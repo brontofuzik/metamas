1c806319514772d8cd98faf00e3b4c7df180b752
lukas.kudela@gmail.com <lukas.kudela@gmail.com@918c96d0-4712-a5f2-c2c5-4549124dd8aa>
2012-01-11 23:17:07 +0000

    Demo1Metamodel: - initial commit.

---
+++ trunk/Demo1Metamodel/src/demo1metamodel/Demo1Metamodel.java
@@ -0,0 +1,16 @@
+package demo1metamodel;
+
+/**
+ * @author Lukáš Kúdela
+ * @since 2012-01-10
+ * @version %I% %G%
+ */
+public class Demo1Metamodel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
