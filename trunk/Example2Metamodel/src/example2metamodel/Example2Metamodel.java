6ffd6557d1a36a5b4d9791987141c630afad6266
lukas.kudela@gmail.com <lukas.kudela@gmail.com@918c96d0-4712-a5f2-c2c5-4549124dd8aa>
2012-01-18 19:00:42 +0000

    Example2Metamodel: - added the project.

---
+++ trunk/Example2Metamodel/src/example2metamodel/Example2Metamodel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example2metamodel;
+
+/**
+ *
+ * @author hp
+ */
+public class Example2Metamodel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
