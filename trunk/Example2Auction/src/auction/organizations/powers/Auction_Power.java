559df8573d184f7b720e7eb46d81a1c9e8ff4b25
lukas.kudela@gmail.com <lukas.kudela@gmail.com@918c96d0-4712-a5f2-c2c5-4549124dd8aa>
2012-01-18 18:43:25 +0000

    Example2Auction: - renamed the project.

---
+++ trunk/Example2Auction/src/auction/organizations/powers/Auction_Power.java
@@ -0,0 +1,18 @@
+package auction.organizations.powers;
+
+import jadeorg.core.organization.Power;
+
+/**
+ * An Auction power.
+ * @author Lukáš Kúdela
+ * @since
+ * @version %I% %G%
+ */
+public class Auction_Power extends Power {
+
+    private static final String NAME = "auction";
+    
+    public Auction_Power() {
+        super(NAME);
+    }
+}
