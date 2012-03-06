38032ebc55f4a5e663465263c36207c132fee707
lukas.kudela <lukas.kudela@gmail.com>
2012-03-06 11:24:15 +0000

    Example1Metamodel: - renaming

--- Example1Metamodel/src/example1metamodel/Example1Metamodel.java
+++ Example1Metamodel/src/example1metamodel/Example1Metamodel.java
@@ -32,85 +32,85 @@ public class Example1Metamodel {
     // ----- PRIVATE -----
     
     private static MultiAgentSystem createModel() {
-        MultiAgentSystem demo1Mas = new MultiAgentSystem("Demo1", "demo1");
+        MultiAgentSystem example1Mas = new MultiAgentSystem("Example1", "example1");
         
         // ---------- Protocols ----------
         
-        // The 'Calculate factorial' protocol.
-        Protocol calculateFactorialProtocol = new Protocol("CalculateFactorialProtocol");
-        demo1Mas.addProtocol(calculateFactorialProtocol);
-        calculateFactorialProtocol.addMessage(new Message("RequestMessage", MessageType.TextMessage));
-        calculateFactorialProtocol.addMessage(new Message("ReplyMessage", MessageType.TextMessage));
+        // The 'Invoke function' protocol.
+        Protocol invokeFunctionProtocol = new Protocol("InvokeFunctionProtocol");
+        example1Mas.addProtocol(invokeFunctionProtocol);
+        invokeFunctionProtocol.addMessage(new Message("RequestMessage", MessageType.TextMessage));
+        invokeFunctionProtocol.addMessage(new Message("ReplyMessage", MessageType.TextMessage));
         
-        // The 'Calculate factorial' initiator party.
-        Party calculateFactorialProtocolInitiatorParty = new Party("CalculateFactorialInitiator");
-        calculateFactorialProtocolInitiatorParty.setFSM(createCalculateFactorialInitiatorFSM());
-        calculateFactorialProtocol.setInitiatorParty(calculateFactorialProtocolInitiatorParty);
+        // The 'Invoke function' initiator party.
+        Party invokeFunctionProtocolInitiatorParty = new Party("InvokeFunction_InitiatorParty");
+        invokeFunctionProtocolInitiatorParty.setFSM(createInvokeFunctionInitiatorFSM());
+        invokeFunctionProtocol.setInitiatorParty(invokeFunctionProtocolInitiatorParty);
         
-        // The 'Calculate factorial' responder party.
-        Party calculateFactorialResponderParty = new Party("CalculateFactorialResponder");
-        calculateFactorialResponderParty.setFSM(createCalculateFactorialResponderFMS());
-        calculateFactorialProtocol.setResponderParty(calculateFactorialResponderParty);
+        // The 'Invoke function' responder party.
+        Party invokeFunctionResponderParty = new Party("InvokeFunctionResponder");
+        invokeFunctionResponderParty.setFSM(createInvokeFunctionResponderFMS());
+        invokeFunctionProtocol.setResponderParty(invokeFunctionResponderParty);
       
         // ---------- Organizations ----------
         
-        // The 'Demo' organization class.
-        OrganizationClass demoOrganizationClass = new OrganizationClass("Demo_Organization");
-        demo1Mas.addOrganizationClass(demoOrganizationClass);
+        // The 'Invoke function' organization class.
+        OrganizationClass invokeFunctionOrganizationClass = new OrganizationClass("Demo_Organization");
+        example1Mas.addOrganizationClass(invokeFunctionOrganizationClass);
              
-        // The 'demo' organization.
-        Organization demoOrganization = demoOrganizationClass.createOrganization("demo_Organization");
-        demo1Mas.addOrganization(demoOrganization);
+        // The 'invoke function' organization.
+        Organization invokeFunctionOrganization = invokeFunctionOrganizationClass.createOrganization("demo_Organization");
+        example1Mas.addOrganization(invokeFunctionOrganization);
         
         // ---------- Roles ----------
         
-        // The 'Asker' role class.
-        RoleClass askerRoleClass = new RoleClass("Asker_Role");
-        demoOrganizationClass.addRole(askerRoleClass);
+        // The 'Invoker' role class.
+        RoleClass invokerRoleClass = new RoleClass("Invoker_Role");
+        invokeFunctionOrganizationClass.addRole(invokerRoleClass);
         
-        // The 'Calculate factorial' power.
-        Power calculateFactorialPower = new Power("CalculateFactorial_Power",
+        // The 'Invoke function' competence.
+        Power invokeFunctionCompetence = new Power("InvokeFunction_Competence",
             Power.PowerType.FSMPower, "Integer", "Integer");
-        calculateFactorialPower.setFSM(createFactorialPowerFSM());
-        askerRoleClass.addPower(calculateFactorialPower);
+        invokeFunctionCompetence.setFSM(createInvokeFunctionCompetenceFSM());
+        invokerRoleClass.addPower(invokeFunctionCompetence);
         
-        // The 'Answerer' role.
-        RoleClass answererRoleClass = new RoleClass("Answerer_Role");
-        demoOrganizationClass.addRole(answererRoleClass);
+        // The 'Executer' role.
+        RoleClass executerRoleClass = new RoleClass("Executer_Role");
+        invokeFunctionOrganizationClass.addRole(executerRoleClass);
         
         // ---------- Players ----------
         
         // The 'Demo' player class.
         PlayerClass demoPlayerClass = new PlayerClass("Demo_Player");
-        demo1Mas.addPlayerClass(demoPlayerClass);
+        example1Mas.addPlayerClass(demoPlayerClass);
         
-        // The 'Calcualate factorial' requirement.
-        Requirement calculateFactorialRequirement = new Requirement("CalculateFactorial_Requirement",
+        // The 'Execute function' responsibility.
+        Requirement executeFunctionResponsibility = new Requirement("InvokeFunction_Responsibility",
             Requirement.RequirementType.OneShotRequirement, "Integer", "Integer");
-        demoPlayerClass.addRequirement(calculateFactorialRequirement);
+        demoPlayerClass.addRequirement(executeFunctionResponsibility);
         
         // The 'demo1' player.
         Player demo1Player = demoPlayerClass.createPlayer("demo1_Player");
-        demo1Mas.addPlayer(demo1Player);
+        example1Mas.addPlayer(demo1Player);
         
         // The 'demo2' player.
         Player demo2Player = demoPlayerClass.createPlayer("demo2_Player");
-        demo1Mas.addPlayer(demo2Player);
+        example1Mas.addPlayer(demo2Player);
         
-        return demo1Mas;
+        return example1Mas;
     }
 
-    private static FSM createFactorialPowerFSM() {
+    private static FSM createInvokeFunctionCompetenceFSM() {
         // TODO Implement.
         throw new UnsupportedOperationException("Not yet implemented");
     }
     
-    private static FSM createCalculateFactorialInitiatorFSM() {
+    private static FSM createInvokeFunctionInitiatorFSM() {
          // TODO Implement.
         throw new UnsupportedOperationException("Not yet implemented");       
     }
     
-    private static FSM createCalculateFactorialResponderFMS() {
+    private static FSM createInvokeFunctionResponderFMS() {
         // TODO Implement.
         throw new UnsupportedOperationException("Not yet implemented");
     }
