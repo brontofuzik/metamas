5f20f6ea6b03b4a143f91d750c89981823798bd7
lukas.kudela@gmail.com <lukas.kudela@gmail.com@918c96d0-4712-a5f2-c2c5-4549124dd8aa>
2012-03-15 19:59:22 +0000

    Example3ExpressionEvaluation: - 'Evaluate expression' state, 'Evaluate expression' responder party and 'Evaluate expression' competence.

--- trunk/Example3ExpressionEvaluation/src/example3/organizations/expressionevaluation/evaluator/Evaluate_Competence.java
+++ trunk/Example3ExpressionEvaluation/src/example3/organizations/expressionevaluation/evaluator/Evaluate_Competence.java
@@ -1,5 +1,6 @@
 package example3.organizations.expressionevaluation.evaluator;
 
+import jade.core.AID;
 import thespian4jade.core.organization.power.FSMPower;
 
 /**
@@ -10,5 +11,33 @@ import thespian4jade.core.organization.power.FSMPower;
  */
 public class Evaluate_Competence extends FSMPower<String, Integer> {
     
+    // <editor-fold defaultstate="collapsed" desc="Fields">
     
+    /**
+     * The AID of the binary evaluator initiating the protocol.
+     */
+    private AID binaryEvaluatorAID;
+    
+    /**
+     * The expression to evaluate.
+     */
+    private String expression;
+    
+    /**
+     * The value of the expression.
+     */
+    private int value;
+    
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Constructors">
+    
+    /**
+     * Initializes a new instance of the Evaluate_Competence class.
+     */
+    public Evaluate_Competence() {
+        
+    }
+    
+    // </editor-fold>
 }
