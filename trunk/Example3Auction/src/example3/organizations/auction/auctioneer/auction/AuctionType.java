018a0980f2d44cba5670e22364656a235b204693
lukas.kudela@gmail.com <lukas.kudela@gmail.com@918c96d0-4712-a5f2-c2c5-4549124dd8aa>
2012-03-18 20:07:00 +0000

    Example3Auction: - TODOs

--- trunk/Example3Auction/src/example3/organizations/auction/auctioneer/auction/AuctionType.java
+++ trunk/Example3Auction/src/example3/organizations/auction/auctioneer/auction/AuctionType.java
@@ -7,37 +7,61 @@ package example3.organizations.auction.auctioneer.auction;
  * @version %I% %G%
  */
 public enum AuctionType {
-    /**
-     * The English auction,
-     * a. k. a. the open-bid ascending price auction.
-     */
-    ENGLISH(0),
-    
-    /**
-     * The Dutch auction,
-     * a. k. a. the open-bid descending price auction
-     */
-    DUTCH(1),
     
     /**
      * The Envelope auction,
      * a. k. a. the sealed-bid first-price auction.
      */
-    ENVELOPE(2),
+    ENVELOPE("Envelop auction"),
     
     /**
      * The Vickrey auction,
      * a. k. a. sealed-bid second-price auction.
      */
-    VICKREY(3);
+    VICKREY("Vickrey auction"),
     
-    private int value;
+    /**
+     * The English auction,
+     * a. k. a. the open-bid ascending price auction.
+     */
+    ENGLISH("English auction"),
     
-    private AuctionType(int value) {
-        this.value = value;
+    /**
+     * The Dutch auction,
+     * a. k. a. the open-bid descending price auction
+     */
+    DUTCH("Dutch auction");
+    
+    // <editor-fold defaultstate="collapsed" desc="Fields">
+    
+    /**
+     * The name of the auction type.
+     */
+    private String name;
+    
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Constructors">
+    
+    /**
+     * Initializes a new instance of the AuctionType enum.
+     * @param name the name of the auction type
+     */
+    private AuctionType(String name) {
+        this.name = name;
     }
     
-    public int getValue() {
-        return value;
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Getters and setters">
+   
+    /**
+     * Gets the name of the auction type.
+     * @return the name of the auction type
+     */
+    public String getName() {
+        return name;
     }
+    
+    // </editor-fold>
 }
