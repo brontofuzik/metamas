2a0973318984053f8407d11b57b39036b0130b0e
lukas.kudela@gmail.com <lukas.kudela@gmail.com@918c96d0-4712-a5f2-c2c5-4549124dd8aa>
2012-03-21 18:54:50 +0000

    Thespian4Jade: - added the Protocol registry (two versions: static class and singleton). - added the Singleton registry (currently not used). - added the Class helper. - removed the Generics helper.

---
+++ branches/subscribe-to-event/Thespian4Jade/src/thespian4jade/proto/ProtocolRegistry_StaticClass.java
@@ -0,0 +1,32 @@
+package thespian4jade.proto;
+
+import java.util.HashMap;
+import java.util.Map;
+import thespian4jade.util.ClassHelper;
+
+/**
+ * A protocol registry - static class version.
+ * @author Lukáš Kúdela
+ * @since 2012-03-21
+ * @version %I% %G%
+ */
+public class ProtocolRegistry_StaticClass {
+    
+    // <editor-fold defaultstate="collapsed" desc="Fields">
+    
+    private static Map<Class, Protocol> protocols = new HashMap<Class, Protocol>();
+    
+    // </editor-fold>   
+    
+    // <editor-fold defaultstate="collapsed" desc="Methods">
+    
+    public static Protocol getProtocol(Class protocolClass) {
+        if (!protocols.containsKey(protocolClass)) {
+            Protocol protocol = ClassHelper.instantiateClass(protocolClass);
+            protocols.put(protocolClass, protocol);
+        }
+        return protocols.get(protocolClass);
+    }
+    
+    // </editor-fold>
+}
