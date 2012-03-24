878cb55c0660e9fbf4151d6e097a1ffe129742bd
lukas.kudela <lukas.kudela@gmail.com>
2012-03-24 10:23:14 +0000

    Example3Auction: - added some API comments.

--- Example3Auction/src/example3/players/Player3.java
+++ Example3Auction/src/example3/players/Player3.java
@@ -3,7 +3,8 @@ package example3.players;
 import thespian4jade.core.Event;
 
 /**
- * The 'Participant3' player.
+ * 'Player1' player - a 'Participant' player playing the 'Auctioneer' role
+ * in the third round and the 'Bidder' role in the first and second rounds.
  * @author Lukáš Kúdela
  * @since 2012-01-20
  * @version %I% %G%
