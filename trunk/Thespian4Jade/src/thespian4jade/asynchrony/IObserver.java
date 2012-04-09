fc785cf9fccf755d77b7bc346a06931f45e9d964
lukas.kudela@gmail.com <lukas.kudela@gmail.com@918c96d0-4712-a5f2-c2c5-4549124dd8aa>
2012-04-09 09:18:20 +0000

    Thespian4Jade: - added API comments for the 'asynchrony' package.

--- trunk/Thespian4Jade/src/thespian4jade/asynchrony/IObserver.java
+++ trunk/Thespian4Jade/src/thespian4jade/asynchrony/IObserver.java
@@ -1,7 +1,8 @@
 package thespian4jade.asynchrony;
 
 /**
- * An observer.
+ * An interface of an object acting as the 'Observer' in the 'Observer'
+ * design pattern.
  * @author Lukáš Kúdela
  * @since 2012-03-19
  * @version %I% %G%
@@ -10,6 +11,10 @@ public interface IObserver {
 
     // <editor-fold defaultstate="collapsed" desc="Methods">
     
+    /**
+     * Updates this observer at the notification from the observable.
+     * @param observable the observable that caused the update
+     */
     public void update(IObservable observable);
     
     // </editor-fold>
