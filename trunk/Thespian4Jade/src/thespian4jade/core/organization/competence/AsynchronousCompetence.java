44e43186f782f495c805a2459af4ac6f53989461
lukas.kudela@gmail.com <lukas.kudela@gmail.com@918c96d0-4712-a5f2-c2c5-4549124dd8aa>
2012-04-09 19:36:50 +0000

    Thespian4Jade: - added API comments for the 'core.organization.competence' package.

--- trunk/Thespian4Jade/src/thespian4jade/core/organization/competence/AsynchronousCompetence.java
+++ trunk/Thespian4Jade/src/thespian4jade/core/organization/competence/AsynchronousCompetence.java
@@ -4,7 +4,7 @@ import thespian4jade.behaviours.states.OneShotBehaviourState;
 import java.io.Serializable;
 
 /**
- * A one-shot competence.
+ * An asynchronous competence.
  * @author Lukáš Kúdela
  * @since 2012-01-02
  * @version %I% %G%
@@ -14,29 +14,51 @@ public abstract class AsynchronousCompetence
     extends OneShotBehaviourState implements ICompetence<TArgument, TResult> {
     
     // <editor-fold defaultstate="collapsed" desc="Fields">
-    
+ 
+    /**
+     * The competence argument.
+     */
     private TArgument argument;
     
+    /**
+     * The competecne result.
+     */
     private TResult result;
     
     // </editor-fold>
     
     // <editor-fold defaultstate="collapsed" desc="Getters and setters">
     
+    /**
+     * Sets the competence argument.
+     * @param argument the competence argument
+     */
     public void setArgument(TArgument argument) {
         this.argument = argument;
     }
     
+    /**
+     * Gets the competence result.
+     * @return the competence result
+     */
     public TResult getResult() {
         return result;
     }
     
     // ----- PROTECTED -----
     
+    /**
+     * Gets the competence argument.
+     * @return the competence argument
+     */
     protected TArgument getArgument() {
         return argument;
     }
     
+    /**
+     * Sets the competence result.
+     * @param result the competence result
+     */
     public void setResult(TResult result) {
         this.result = result;
     }
